576026d8e9a0d46033dd6c53248d2090f34419fe
Kevin <Kevin@DESKTOP-KEVIN>
2017-03-22 13:23:27 +0100

    Add project files.

---
+++ Project_Stilla/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_Stilla.Startup))]
+namespace Project_Stilla
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
